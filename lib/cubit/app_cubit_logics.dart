1eef983c38a1b8ec99d1d9b3b6039ac452dac3af
Saif Elden Khaled <saifkhaledd1511@gmail.com>
2024-01-04 10:05:48 +0200

    integerated login signup welcome with cubit

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -20,7 +20,7 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
       body: BlocBuilder<AppCubits, CubitStates>(
         builder: (context, state) {
           if (state is WelcomeState) {
-            return WelcomePagee();
+            return WelcomePage();
           }
           if (state is DetailState) {
             return DetailPage();
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,99 +1,151 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_homsa/cubit/app_cubits.dart';
-import 'package:test_homsa/widgets/app_large_text.dart';
-import 'package:test_homsa/widgets/app_text.dart';
 import 'package:test_homsa/misc/colors.dart';
 import 'package:test_homsa/widgets/responsive_button.dart';
+import 'navpages/login.dart';
+import 'navpages/signup.dart';
 
-class WelcomePagee extends StatefulWidget {
-  const WelcomePagee({Key? key}) : super(key: key);
-  @override
-  _WelcomePageeState createState() => _WelcomePageeState();
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: WelcomePage(),
+  ));
 }
 
-class _WelcomePageeState extends State<WelcomePagee> {
-  List images = [
-    "welcome-one.png",
-    "welcome-two.png",
-    "welcome-three.png",
-  ];
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: images.length,
-          itemBuilder: (_, index) {
-            return Container(
-                width: double.maxFinite,
-                height: double.maxFinite,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("img/" + images[index]),
-                        fit: BoxFit.cover)),
-                child: Container(
-                    margin:
-                        const EdgeInsets.only(top: 150, left: 20, right: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            AppLargeText(text: "Discover"),
-                            AppText(
-                              text: "Egypt",
-                              size: 30,
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              width: 250,
-                              child: AppText(
-                                  text: "egypt is a very very niceee placeeeee",
-                                  color: AppColors.textColor2,
-                                  size: 14),
-                            ),
-                            SizedBox(
-                              height: 40,
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                BlocProvider.of<AppCubits>(context).getData();
-                              },
-                              child: Container(
-                                width: 200,
-                                child: Row(
-                                  children: [
-                                    ResponsiveButton(
-                                      width: 120,
-                                    ),
-                                  ],
-                                ),
+      body: Stack(
+        children: [
+          // Background Image
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("img/welcome-one.png"), // Use the first image
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+
+          // Content
+          SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      // AppLargeText(text: "PyraGuide"),
+                      SizedBox(height: 2),
+                      // You can add a subheading here if needed
+                    ],
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("img/welcome-three.png"), // Use the third image
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          BlocProvider.of<AppCubits>(context).getData();
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.black, // Change the color as needed
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Discover Egypt",
+                              style: TextStyle(
+                                fontFamily: 'Inter',
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
                               ),
-                            )
-                          ],
+                            ),
+                          ),
                         ),
-                        Column(
-                          children: List.generate(3, (indexDots) {
-                            return Container(
-                              margin: const EdgeInsets.only(bottom: 2),
-                              width: 8,
-                              height: index == indexDots ? 25 : 8,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: index == indexDots
-                                      ? AppColors.mainColor
-                                      : AppColors.mainColor.withOpacity(0.3)),
-                            );
-                          }),
-                        )
-                      ],
-                    )));
-          }),
+                      ),
+                      SizedBox(height: 20),
+                      MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignupPage(),
+                            ),
+                          );
+                        },
+                        color: Color(0xffDAA520),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
